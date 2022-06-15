39dba449a0497980bb70fbd4b79cc4ff480936cb
ricoatsproutasia <arisandy.rico@sproutasia.com>
2022-06-15 10:04:39 +0700

    feat: include our route

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -1,16 +1,23 @@
-@include('layout.header')
-<div>
-    <a href="/contacts/create" class="btn btn-primary">Add</a>
-    <table class="table">
-        <thead>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Phone</th>
-        </thead>
-        <tbody>
-            <?php foreach ($contacts as $c) { ?>
-                @include('contact.item', $c)
-            <?php } ?>
-        </tbody>
-    </table>
-</div>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+    @include('layout.header')
+    <div class="p-4">
+        <a href="/contacts/create" class="btn btn-primary">Add</a>
+        <table class="table">
+            <thead>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Phone</th>
+            </thead>
+            <tbody>
+                <?php foreach ($contacts as $c) { ?>
+                    @include('contact.item', $c)
+                <?php } ?>
+            </tbody>
+        </table>
+    </div>
+</x-app-layout>
